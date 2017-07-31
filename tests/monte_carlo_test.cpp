b70650f6f8d7a95d2b84ff1c91cba43a828e25e8
Demiralp <demiralpali@gmail.com>
2017-08-01 00:15:05 +0200

    Using double literals in monte_carlo_test.

--- tests/monte_carlo_test.cpp
+++ tests/monte_carlo_test.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Monte Carlo estimator is tested.", "[monte_carlo_estimator]")
   monte_carlo<double> pi_estimator(
     [] (const std::function<double()>& rng)
     {
-      return sqrtf(pow(rng(), 2) + pow(rng(), 2)) <= 1.0F ? 1.0F : 0.0F;
+      return sqrtf(pow(rng(), 2) + pow(rng(), 2)) <= 1.0 ? 1.0 : 0.0;
     });
   WARN("Pi is estimated as " << pi_estimator.simulate(30000) * 4);
 }
